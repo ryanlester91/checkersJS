008d5760d9c60982ff94c5c3bbb49c24aecb757c
Ryan Lester <lestertr@dukes.jmu.edu>
2020-07-17 20:07:39 -0400

    cell even and odd js function

--- template.js
+++ template.js
@@ -1,26 +1,27 @@
 //let cell11 = `<div id="cell-1-1" class="cell white"></div>`
 
 function cell(rowNum, cellNum) {
-   console.log(cellNum % 2)
-    console.log(`writing cell ${rowNum, cellNum}`)
+   //console.log(cellNum % 2)
+   let even = true
+   if (cellNum % 2 == 1) even = false
+    console.log(`writing cell ${rowNum, cellNum} and even is ${even}`)
+    if (even) {
+        return `
+        <div id="cell-1-2" class="cell black">
+            <div class="checker white-checker"></div>
+        </div>
+        `
+    } else
     return `<div id="cell-1-1" class="cell white"></div>`
 }
 
 let rowTemplate = `
     ${cell(1,1)}
-    <div id="cell-1-2" class="cell black">
-        <div class="checker white-checker"></div>
-    </div>
+    ${cell(1,2)}
     ${cell(1,3)}
-    <div id="cell-1-4" class="cell black">
-        <div class="checker white-checker"></div>
-    </div>
+    ${cell(1,4)}
     ${cell(1,5)}
-    <div id="cell-1-6" class="cell black">
-        <div class="checker white-checker"></div>
-    </div>
+    ${cell(1,6)}
     ${cell(1,7)}
-    <div id="cell-1-8" class="cell black">
-        <div class="checker white-checker"></div>
-    </div>
+    ${cell(1,8)}
 `
