3fa6313d1b522af14991860606ac96a009ad899a
Ryan Lester <lestertr@dukes.jmu.edu>
2020-07-25 19:07:16 -0400

    revising template functions and trimmed down HTML code

--- template.js
+++ template.js
@@ -1,18 +1,22 @@
 //let cell11 = `<div id="cell-1-1" class="cell white"></div>`
 
-function cell(rowNum, cellNum) {
+function cellTemplate(rowNum, cellNum) {
    //console.log(cellNum % 2)
-   let even = true
-   if (cellNum % 2 == 1) even = false
-    console.log(`writing cell ${rowNum, cellNum} and even is ${even}`)
-    if (even) {
+   var isCellEven = true
+   var isRowEven = true
+   if (cellNum % 2 == 1) isCellEven = false
+   if (rowNum % 2 == 1) isRowEven = false
+    //console.log(`writing cell ${rowNum, cellNum} and even is ${even}`)
+    if (isCellEven != isRowEven) {
+        //Black Cell
         return `
-        <div id="cell-1-${cellNum}" class="cell black">
+        <div id="cell-${rowNum}-${cellNum}" class="cell black">
             <div class="checker white-checker"></div>
         </div>
         `
     } else
-    return `<div id="cell-1-${cellNum}" class="cell white"></div>`
+        //White Cell
+    return `<div id="cell-${rowNum}-${cellNum}" class="cell white"></div>`
 }
 
 /*let rowTemplate = `
@@ -25,11 +29,25 @@ function cell(rowNum, cellNum) {
     ${cell(1,7)}
     ${cell(1,8)}
 `*/
-function rowTemplate() {
-    let rowString = ''
-    for(let cellNum=1; cellNum<=8; cellNum++) {
-        rowString = rowString + cell(1, cellNum)
-        console.log(rowString);
+function rowTemplate(rowNum) {
+    var rowString = ''
+    console.log(`drawing row number ${rowNum}`);
+    for(var cellNum=1; cellNum<=8; cellNum++) {
+        rowString = rowString + `<div id="row-${rowNum}" class="row">`
+        rowString = rowString + cellTemplate(rowNum, cellNum)
+        rowString = rowString + `</div>`
+        //console.log(rowString);
     }
     return rowString
+}
+
+function boardTemplate() {
+    var boardString = ''
+    for(var rowNum=1; rowNum<=8; rowNum++) {
+        
+        boardString = boardString + rowTemplate(rowNum);
+        
+        //console.log(boardString);
+    }
+    return boardString
 }
